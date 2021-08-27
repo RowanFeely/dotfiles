4bfab9a00a638cc2c34d961c1e72e3b260c5526f
Rowan Feely <RowanFeely@users.noreply.github.com>
2021-08-28 01:36:51 +1000

    rebuilt banner gen

---
+++ zsh/.zsh/00-banner-gen.zsh
@@ -0,0 +1,65 @@
+#                 ██
+#                ░██
+#  ██████  ██████░██
+# ░░░░██  ██░░░░ ░██████
+#    ██  ░░█████ ░██░░░██
+#   ██    ░░░░░██░██  ░██
+#  ██████ ██████ ░██  ░██
+# ░░░░░░ ░░░░░░  ░░   ░░
+#
+#  ▓▓▓▓▓▓▓▓▓▓
+# ░▓ author ▓ ROGUE(X3) <rowanfeely.github.io>
+# ░▓   code ▓ https://github.com/RowanFeely/dotfiles
+# ░▓▓▓▓▓▓▓▓▓▓
+# ░░░░░░░░░░
+
+#█▓▒░ pick a random number
+_RAND=`shuf -i1-11 -n1`
+
+#█▓▒░ display a random ascii banner
+case $_RAND in
+1)
+  clear
+  ansicat < "/Users/rowanfeely/Repos/Banner-gen/banners/+l-fd.ans"
+;;
+2)
+  clear
+  ansicat < "/Users/rowanfeely/Repos/Banner-gen/banners/grk-chkl.ans"
+;;
+3)
+  clear
+  ansicat < "/Users/rowanfeely/Repos/Banner-gen/banners/grk-usrl.ans"
+;;
+4)
+  clear
+  ansicat < "/Users/rowanfeely/Repos/Banner-gen/banners/hx-askee.ans"
+;;
+5)
+  clear
+  ansicat < "/Users/rowanfeely/Repos/Banner-gen/banners/hx-askee.ans"
+;;
+6)
+  clear
+  ansicat < "/Users/rowanfeely/Repos/Banner-gen/banners/jp!xib1.ans"
+;;
+7)
+  clear
+  ansicat < "/Users/rowanfeely/Repos/Banner-gen/banners/pmt-bos.ans"
+;;
+8)
+  clear
+  ansicat < "/Users/rowanfeely/Repos/Banner-gen/banners/po-pog1.ans"
+;;
+9)
+  clear
+  ansicat < "/Users/rowanfeely/Repos/Banner-gen/banners/us-bbsln.ans"
+;;
+10)
+  clear
+  ansicat < "/Users/rowanfeely/Repos/Banner-gen/banners/us-warez.ans"
+;;
+11)
+  clear
+  ansicat < "/Users/rowanfeely/Repos/Banner-gen/banners/xz-impure.ans"
+;;
+esac
